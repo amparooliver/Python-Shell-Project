e893951ee17bc8b631141c4397890838abb8aea9
aoliver <you@example.com>
2021-12-29 13:29:20 -0300

    Base ir, base creardir

--- shell.py
+++ shell.py
@@ -1,34 +1,134 @@
+import getpass
 import os
-import getpass 
-import ir 
+import shlex
+import signal
+import socket
+import subprocess
+import sys
 
-##################################################
-# Controls the flow of the program (main)
-##################################################
+from constants import SHELL_STATUS_RUN, SHELL_STATUS_STOP
+
+# FUNCIONES #
+########################################
+def exits(args):
+    return SHELL_STATUS_STOP
+########################################
+def ir(args):
+    if len(args) > 0:
+        os.chdir(args[0])
+    else:
+        os.chdir(os.getenv('HOME'))
+    return SHELL_STATUS_RUN
+########################################
+def creardir(args):
+    try: 
+        os.mkdir(args[0]) 
+    except OSError as error: 
+        print(error)  
+    return SHELL_STATUS_RUN
+########################################
+#def listar(args):
+
+
+# Hash map to store built-in function name and reference as key and value
+built_in_cmds = {}
+
+def tokenize(string):
+    return shlex.split(string)
+
+def preprocess(tokens):
+    processed_token = []
+    for token in tokens:
+        # Convert $-prefixed token to value of an environment variable
+        if token.startswith('$'):
+            processed_token.append(os.getenv(token[1:]))
+        else:
+            processed_token.append(token)
+    return processed_token
+
+
+def handler_kill(signum, frame):
+    raise OSError("Killed!")
+
+def execute(cmd_tokens):
+
+    if cmd_tokens:
+        # Extract command name and arguments from tokens
+        cmd_name = cmd_tokens[0]
+        cmd_args = cmd_tokens[1:]
+
+        # If the command is a built-in command,
+        # invoke its function with arguments
+        if cmd_name in built_in_cmds:
+            return built_in_cmds[cmd_name](cmd_args)
+
+        # Wait for a kill signal
+        signal.signal(signal.SIGINT, handler_kill)
+        # Spawn a child process
+        # Unix support
+        p = subprocess.Popen(cmd_tokens)
+        # Parent process read data from child process
+        # and wait for child process to exit
+        p.communicate()
+
+    # Return status indicating to wait for next command in shell_loop
+    return SHELL_STATUS_RUN
+
+
+#Display a command prompt as `[<user>@<hostname> <dir>]$ `
+def display_cmd_prompt():
+    # Get user and hostname
+    user = getpass.getuser()
+    hostname = socket.gethostname()
+
+    # Get base directory (last part of the curent working directory path)
+    cwd = os.getcwd()
+    base_dir = os.path.basename(cwd)
+
+    # Use ~ instead if a user is at his/her home directory
+    home_dir = os.path.expanduser('~')
+    if cwd == home_dir:
+        base_dir = '~'
+
+    # Print out to console
+    sys.stdout.write("[%s@%s %s]$ " % (user, hostname, base_dir))
+    sys.stdout.flush()
+
+def shell_loop():
+    status = SHELL_STATUS_RUN
+
+    while status == SHELL_STATUS_RUN:
+        display_cmd_prompt()
+
+        try:
+            # Read command input
+            cmd = sys.stdin.readline()
+            # Tokenize the command input
+            cmd_tokens = tokenize(cmd)
+            # Preprocess special tokens
+            # (e.g. convert $<env> into environment value)
+            cmd_tokens = preprocess(cmd_tokens)
+            # Execute the command and retrieve new status
+            status = execute(cmd_tokens)
+        except:
+            _, err, _ = sys.exc_info()
+            print(err)
+
+
+# Register a built-in function to built-in command hash map
+def register_command(name, func):
+    built_in_cmds[name] = func
+
+# Register all built-in commands here
+def init():
+    register_command("ir", ir)
+    register_command("creardir", creardir)
+    register_command("exit", exits)
 
 def main():
+    # Init shell before starting the main loop
+    init()
+    shell_loop()
 
-    # Infinite loop til user input = exit
-    while(True):
-        username = getpass.getuser() # To obtain the “login name” of the user.
-        computer = "@" + os.uname().nodename # To obtain computer name
-        current_path = ":" + os.getcwd() + "$ " # To get current path
-        user_input = (input( username + computer + current_path )).split() # Read user input
-        if(user_input == 'exit'):
-            break
-        else:
-            run_command(user_input)
-
-##################################################
-# Searches the right command and executes it
-# TIP: Indexing Syntax for Slicing obj[start:stop:step]
-##################################################
-
-def run_command(user_input):
-
-    if user_input[:3] == "ir":
-        # Verificaciones
-        ir(user_input[3:])  
-
-##################################################
-
+if __name__ == "__main__":
+    main()
